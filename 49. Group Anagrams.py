cee4b1ed7d9799cc4351716eaf2deb694f362f4d
Slava Maltsev <slavaxru@gmail.com>
2023-01-18 14:46:00 +0100

    done

---
+++ 49. Group Anagrams.py
@@ -0,0 +1,10 @@
+import collections
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        res = collections.defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return res.values()
